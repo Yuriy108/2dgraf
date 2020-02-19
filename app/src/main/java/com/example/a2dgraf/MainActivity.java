2a03200d6059d0b721659fe36d5317bc6c6e8fd8
Yuriy108 <49155848+Yuriy108@users.noreply.github.com>
2020-02-19 22:50:03 +0200

    Initial commit 1

---
+++ app/src/main/java/com/example/a2dgraf/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.a2dgraf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new MyDraw(this));
+        // setContentView(R.layout.activity_main);
+    }
+}
+
